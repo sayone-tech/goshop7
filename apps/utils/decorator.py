b2c8e5163f681809fd0707246879b5a4ea85581e
savadppns <savadppns@gmail.com>
2014-11-13 11:20:23 +0530

    style changes by biyas

--- apps/utils/decorator.py
+++ apps/utils/decorator.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.decorators import user_passes_test, login_required
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
 
 from apps.utils.views import get_profile
 
@@ -7,4 +9,6 @@ not_a_demo_user_required = user_passes_test(lambda u: not get_profile(u).groups.
 
 def not_a_demo_user(view_func):
     decorated_view_func = login_required(not_a_demo_user_required(view_func))
+#    def _wrapped_view(request, *args, **kwargs):
+#        messages.success(request, _('Demo user has no permission to delete item.'))
     return decorated_view_func
