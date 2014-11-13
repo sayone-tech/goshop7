e1a5b3a63595db3a3953e6e65a330a939c1e4d51
savadppns <savadppns@gmail.com>
2014-11-13 19:26:20 +0530

    decorator for demo user permission changed

--- apps/utils/decorator.py
+++ apps/utils/decorator.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.decorators import user_passes_test, login_required
 from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
+from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 
 from apps.utils.views import get_profile
 
@@ -8,7 +11,9 @@ not_a_demo_user_required = user_passes_test(lambda u: not get_profile(u).groups.
                                    login_url='/dashboard/')
 
 def not_a_demo_user(view_func):
-    decorated_view_func = login_required(not_a_demo_user_required(view_func))
-#    def _wrapped_view(request, *args, **kwargs):
-#        messages.success(request, _('Demo user has no permission to delete item.'))
-    return decorated_view_func
+    def _wrapped_view(request, *args, **kwargs):
+        if request.user.groups.filter(name="Demo").exists():
+            messages.success(request, _('Demo user has no permission to delete item.'))
+            return HttpResponseRedirect('/dashboard/')
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view
