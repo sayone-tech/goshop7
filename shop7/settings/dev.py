64257d5ad407d6bf3c279e5d81562f75a752e521
vineeth-sayone <you@example.com>
2020-05-14 17:23:18 +0530

    email host credentials and google tag changed

--- shop7/settings/dev.py
+++ shop7/settings/dev.py
@@ -1,6 +1,10 @@
+import os
+
 from base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
+from shop7.settings.base import BASE_DIR
+
 DEBUG = True
 
 TEMPLATE_DEBUG = True
@@ -26,7 +30,16 @@ EMAIL_BACKEND = "mailer.backend.DbBackend"
 EMAIL_SUBJECT_PREFIX = "[Shop7]"
 
 EMAIL_HOST          = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = 'developer@sayone'
-EMAIL_HOST_USER     = 'developer.sayone@gmail.com'
+EMAIL_HOST_PASSWORD = 'S4nYNTDs'
+EMAIL_HOST_USER     = 'helloshop7'
 EMAIL_PORT          = 587
 EMAIL_USE_TLS       = True
+
+OSCAR_FROM_EMAIL = 'Team Shop7 <hello@goshop7.com>'
+DEFAULT_FROM_EMAIL = 'Team Shop7 <hello@goshop7.com>'
+
+GOOGLE_ANALYTICS_PROPERTY_ID = 'UA-43709275-2'
+
+GOOGLE_ANALYTICS_DOMAIN = 'http://www.goshop7.com/'
+
+GEOIP_PATH = os.path.join(BASE_DIR, 'geoip')
